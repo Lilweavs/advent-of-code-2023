3c1dfcff3c12dd9746ac83282d177c859b233025
LilWeavs <andrewjw827@gmail.com>
2023-12-05 08:40:06 -0600

    day03 part 1

--- day03/main.zig
+++ day03/main.zig
@@ -1,10 +1,38 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const part1 = try solve(@embedFile("test.txt"));
+    const part1 = try solve(@embedFile("input.txt"));
     std.debug.print("Day 02|1: {d}\n", .{part1});
     // const part2 = solvePart2(@embedFile("input.txt"));
     // std.debug.print("Day 01|2: {d}\n", .{part2});
+}
+
+fn parseNumber(str: *std.ArrayList(u8), idx: usize) usize {
+    if (!std.ascii.isDigit(str.items[idx])) return 0;
+    var left: usize = idx;
+    var right: usize = idx;
+    while (right < str.items.len and std.ascii.isDigit(str.items[right])) {
+        right += 1;
+    }
+    while (left != 0 and std.ascii.isDigit(str.items[left - 1])) {
+        left -= 1;
+    }
+
+    var retval = std.fmt.parseInt(usize, str.items[left..right], 10) catch 0;
+
+    for (left..right) |i| {
+        str.items[i] = '.';
+    }
+
+    std.debug.print("{d}\n", .{retval});
+    return retval;
+}
+
+fn print(grid: *std.ArrayList(std.ArrayList(u8))) void {
+    std.debug.print("\n", .{});
+    for (grid.items) |line| {
+        std.debug.print("{s}\n", .{line.items});
+    }
 }
 
 fn solve(input: []const u8) !usize {
@@ -13,47 +41,58 @@ fn solve(input: []const u8) !usize {
 
     var lines = std.mem.tokenize(u8, input, "\r\n");
 
-    // var schematic = std.ArrayList(u8).init(allocator);
-
     var schematic = std.ArrayList(std.ArrayList(u8)).init(allocator);
 
-    // var rows: usize = 0;
     while (lines.next()) |line| {
         try schematic.append(std.ArrayList(u8).init(allocator));
-        var tmp = schematic.getLast();
-        try tmp.appendSlice(line);
-        // rows = line.len;
-        // try schematic.appendSlice(line);
+        try schematic.items[schematic.items.len - 1].appendSlice(line);
     }
 
-    for (schematic.items) |line| {
-        std.debug.print("{s}\n", .{line.items});
+    var total: usize = 0;
+    // std.debug.print("\n{d}\n", .{total});
+    for (0..schematic.items.len) |i| {
+        for (0..schematic.items[i].items.len) |j| {
+            const c = schematic.items[i].items[j];
+            if (c == '.' or std.ascii.isAlphanumeric(c)) continue;
+            if (j > 0) {
+                total += parseNumber(&schematic.items[i], j - 1);
+            } // left
+
+            if (j < schematic.items[i].items.len - 1) {
+                total += parseNumber(&schematic.items[i], j + 1);
+            } // right
+
+            if (i > 0) {
+                total += parseNumber(&schematic.items[i - 1], j);
+            } // up
+
+            if (i < schematic.items.len - 1) {
+                total += parseNumber(&schematic.items[i + 1], j);
+            } // down
+
+            if (j > 0 and i > 0) {
+                total += parseNumber(&schematic.items[i - 1], j - 1);
+            } // left up
+
+            if ((j < schematic.items[i].items.len - 1) and i > 0) {
+                total += parseNumber(&schematic.items[i - 1], j + 1);
+            } // right up
+
+            if (j > 0 and i < (schematic.items.len - 1)) {
+                total += parseNumber(&schematic.items[i + 1], j - 1);
+            } // left down
+
+            if ((j < schematic.items[i].items.len - 1) and i < (schematic.items.len - 1)) {
+                total += parseNumber(&schematic.items[i + 1], j + 1);
+            } // right down
+        }
+        // print(&schematic);
+
     }
+    return total;
+}
 
-    // std.debug.print("{s}\n", .{schematic.items});
-
-    // var total: usize = 0;
-    // for (1..schematic.len-1) |c, i| {
-    //     if (c != '.' and (c < 48 and c > 57)) {
-
-    //         // \|/ - . - /|\
-
-    //         // up-left
-    //         if ((i - rows - 1)) {
-
-    //         }
-    //         // Up
-
-    //         // up-right
-
-    //         if std.ascii.isDigit();
-
-    //     }
-    // }
-
-    // while (lines.next()) |line| {
-    //     _ = line;
-    // }
-
-    return 0;
+test "test-part1" {
+    const result = try solve(@embedFile("test.txt"));
+    try std.testing.expectEqual(result, 4361);
 }
