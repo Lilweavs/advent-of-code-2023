526b4ead7bd652b64ae3217195a7cf0cb2fa3a5d
LilWeavs <andrewjw827@gmail.com>
2023-12-09 11:19:02 -0600

    day05 part1

--- day05/main.zig
+++ day05/main.zig
@@ -1,15 +1,13 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const part1 = try solve(@embedFile("test.txt"));
+    const part1 = try solve(@embedFile("input.txt"));
     std.debug.print("Day 02|1: {d}\n", .{part1});
     // const part2 = try solve(@embedFile("input.txt"), 2);
     // std.debug.print("Day 01|2: {d}\n", .{part2});
 }
 
-const Ranges = struct { src: usize = 0, des: usize = 0, len: usize = 0 };
-
-const Bounds = struct { lSrc: usize = 0, rSrc: usize = 0, lDes: usize = 0, rDes: usize = 0 };
+const Ranges = struct { des: usize = 0, src: usize = 0, len: usize = 0 };
 
 fn solve(input: []const u8) !usize {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -24,50 +22,39 @@ fn solve(input: []const u8) !usize {
     var nums = std.mem.tokenizeScalar(u8, lines.next().?, ' ');
     _ = nums.next();
     while (nums.next()) |line| {
-        try seeds.append(try std.fmt.parseInt(u8, line, 10));
+        try seeds.append(try std.fmt.parseInt(usize, line, 10));
     } else _ = nums.next();
 
-    var i: usize = 0;
-    while (nums.next()) |line| {
-        if (std.ascii.isAlphabetic(line[0])) continue;
-        if (line.len == 0) {
+    var i: i32 = -1;
+    while (lines.next()) |line| {
+        if (std.ascii.isAlphabetic(line[0])) {
             i += 1;
             continue;
         }
 
         nums = std.mem.tokenizeScalar(u8, line, ' ');
 
-        try maps[i].append(.{ .src = try std.fmt.parseInt(u8, nums.next().?, 10), .des = try std.fmt.parseInt(u8, nums.next().?, 10), .len = try std.fmt.parseInt(u8, nums.next().?, 10) });
-
-        // while (nums.next()) |num| {
-        //     try maps[i].append(try std.fmt.parseInt(u8, num, 10));
-        // }
+        try maps[@intCast(i)].append(.{ .des = try std.fmt.parseInt(usize, nums.next().?, 10), .src = try std.fmt.parseInt(usize, nums.next().?, 10), .len = try std.fmt.parseInt(usize, nums.next().?, 10) });
     }
 
-    try GetUniqueNumbers(maps[0], allocator);
+    var min: usize = std.math.maxInt(usize);
+    for (seeds.items) |seed| {
+        min = @min(min, GetLocation(&maps, seed));
+    }
 
-    return 0;
+    return min;
 }
 
-fn GetUniqueNumbers(map: std.ArrayList(Ranges), allocator: std.mem.Allocator) !void {
-    var uniqueNumbers = std.ArrayList(Bounds).init(allocator);
-
-    for (map.items) |range| {
-        var leftSrc = range.src;
-        var leftDes = range.des;
-        var rightSrc = leftSrc + range.len - 1;
-        var rightDes = rightSrc + range.len - 1;
-
-        for (uniqueNumbers.items) |bounds| {
-            if (bounds.lSrc >= leftSrc) leftSrc = bounds.lSrc + 1;
-            if (bounds.lDes >= leftDes) leftDes = bounds.lDes + 1;
-
-            if (bounds.rSrc <= rightSrc) rightSrc = bounds.rSrc - 1;
-            if (bounds.rDes <= rightDes) rightDes = bounds.rDes - 1;
-
-            if ((rightSrc < leftSrc) or (rightDes < leftDes)) break;
-        } else {
-            try uniqueNumbers.append(.{ .lSrc = leftSrc, .rSrc = rightSrc, .lDes = leftDes, .rDes = rightDes });
+fn GetLocation(maps: *[7]std.ArrayList(Ranges), seed: usize) usize {
+    var retVal = seed;
+    for (maps) |ranges| {
+        for (ranges.items) |range| {
+            if ((retVal >= range.src) and (retVal <= range.src + range.len - 1)) {
+                retVal = retVal - range.src + range.des;
+                break;
+            }
         }
     }
+
+    return retVal;
 }
