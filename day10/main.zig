39bd885d2f7f31b775197a17847e79d6992f7d0f
saltymrbighead <35853304+saltymrbighead@users.noreply.github.com>
2023-12-23 20:38:39 -0600

    day10 part1

--- day10/main.zig
+++ day10/main.zig
@@ -2,21 +2,65 @@ const std = @import("std");
 
 pub fn main() !void {
     const part1 = try solve(@embedFile("input.txt"), 1);
-    std.debug.print("Day 02|1: {d}\n", .{part1});
+    std.debug.print("Day 10|1: {d}\n", .{part1});
 }
 
-const Grid = struct { x: usize, y: usize };
+const Grid = struct { x: isize = undefined, y: isize = undefined };
 
-fn FollowPipe(loc: Grid, dir: u8) [2]Grid {
-    return switch (dir) { // +1 south, -1 north
-        '|' => .{ Grid{ .x = loc.x, .y = loc.y + 1 }, Grid{ .x = loc.x, .y = loc.y - 1 } },
-        '-' => .{ Grid{ .x = loc.x + 1, .y = loc.y }, Grid{ .x = loc.x - 1, .y = loc.y } },
-        'L' => .{ Grid{ .x = loc.x, .y = loc.y - 1 }, Grid{ .x = loc.x + 1, .y = loc.y } },
-        'J' => .{ Grid{ .x = loc.x, .y = loc.y - 1 }, Grid{ .x = loc.x - 1, .y = loc.y } },
-        '7' => .{ Grid{ .x = loc.x, .y = loc.y + 1 }, Grid{ .x = loc.x - 1, .y = loc.y } },
-        'F' => .{ Grid{ .x = loc.x, .y = loc.y + 1 }, Grid{ .x = loc.x + 1, .y = loc.y } },
+const Direction = enum { North, South, East, West, Stop };
+
+const Starts = struct { pipe: u8, dir: Direction };
+
+const startPipes = [_]u8{ '|', '-', 'L', 'J', '7', 'F' };
+const startDir = [_]Direction{ Direction.North, Direction.West, Direction.North, Direction.North, Direction.South, Direction.South };
+
+fn FollowPipe(loc: Grid, dir: Direction) Grid {
+    return switch (dir) {
+        Direction.North => return .{ .x = loc.x, .y = loc.y - 1 },
+        Direction.South => return .{ .x = loc.x, .y = loc.y + 1 },
+        Direction.East => return .{ .x = loc.x + 1, .y = loc.y },
+        Direction.West => return .{ .x = loc.x - 1, .y = loc.y },
         else => unreachable,
     };
+}
+
+fn GetNextDirection(nextp: u8, direction: Direction) ?Direction {
+    // from direction
+    switch (direction) {
+        Direction.North => {
+            return switch (nextp) {
+                '|' => Direction.North,
+                '7' => Direction.West,
+                'F' => Direction.East,
+                else => null,
+            };
+        },
+        Direction.South => {
+            return switch (nextp) {
+                '|' => Direction.South,
+                'L' => Direction.East,
+                'J' => Direction.West,
+                else => null,
+            };
+        },
+        Direction.East => {
+            return switch (nextp) {
+                '-' => Direction.East,
+                'J' => Direction.North,
+                '7' => Direction.South,
+                else => null,
+            };
+        },
+        Direction.West => {
+            return switch (nextp) {
+                '-' => Direction.West,
+                'F' => Direction.South,
+                'L' => Direction.North,
+                else => null,
+            };
+        },
+        else => unreachable,
+    }
 }
 
 fn solve(input: []const u8, comptime part: usize) !isize {
@@ -26,25 +70,53 @@ fn solve(input: []const u8, comptime part: usize) !isize {
 
     var lines = std.mem.tokenize(u8, input, "\r\n");
 
-    var map = std.mem.AutoHashMap(Grid, u8).init(allocator);
+    var map = std.AutoHashMap(Grid, u8).init(allocator);
 
-    var start = Grid{};
+    var start = Grid{ .x = undefined, .y = undefined };
 
     var i: usize = 0;
     while (lines.next()) |line| : (i += 1) {
         for (line, 0..) |c, j| {
-            if (c == 'S') start = Grid{ .x = j, .y = i };
-            if (c != '.') try map.put(.{ i, j }, c);
+            if (c == 'S') start = Grid{ .x = @intCast(j), .y = @intCast(i) };
+
+            try map.put(.{ .x = @intCast(j), .y = @intCast(i) }, c);
+
+            // if (c != '.') try map.put(.{ .x = @intCast(j), .y = @intCast(i) }, c);
         }
     }
 
-    var visited = std.mem.AutoHashMap(Grid, u8).init(allocator);
-    var needToVisit = std.AutoHashMap(Grid, u8).init(allocator);
-    _ = needToVisit;
+    var maxDis: isize = 0;
+    for (startPipes, startDir) |sp, sd| {
+        const tmp = try FindPath(&map, start, sd, sp, allocator) orelse continue;
+        maxDis = @max(maxDis, tmp);
+        // std.debug.print("{c},{},{}\n", .{ sp, sd, tmp });
+    }
+
+    return maxDis;
+}
+
+fn FindPath(map: *std.AutoHashMap(Grid, u8), start: Grid, sd: Direction, sp: u8, allocator: std.mem.Allocator) !?isize {
+    var direction = sd; // current direction
+    var pipe = sp;
     var curr = start;
-    _ = curr;
 
-    while (!visited.contains(start)) {}
+    var path = std.ArrayList(Grid).init(allocator);
 
-    return 0;
+    try map.put(start, pipe);
+
+    var k: usize = 0;
+    while (true) : (k += 1) {
+        curr = FollowPipe(curr, direction);
+
+        pipe = map.get(curr) orelse return null;
+
+        direction = GetNextDirection(pipe, direction) orelse return null;
+
+        try path.append(curr);
+        // std.debug.print("{d},{c}\n", .{ k, pipe });
+        if (start.x == curr.x and start.y == curr.y) break;
+    }
+
+    // std.debug.print("length: {d}\n", .{path.items.len});
+    return @intCast(path.items.len / 2);
 }
