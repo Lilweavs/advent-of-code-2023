454932d76b55b1d2a47dd0b3505b598be29eb683
LilWeavs <andrewjw827@gmail.com>
2024-01-02 22:23:27 -0600

    day14 part2

--- day14/main.zig
+++ day14/main.zig
@@ -1,134 +1,129 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const part1 = try solve(@embedFile("test.txt"), 1);
-    std.debug.print("Day 13|1: {d}\n", .{part1});
+    const part1 = try solve(@embedFile("input.txt"), 1);
+    std.debug.print("Day 14|1: {d}\n", .{part1});
     // const part2 = try solve(@embedFile("test.txt"), 2);
     // std.debug.print("Day 13|2: {d}\n", .{part2});
 }
-
-const Direction = enum { North, South, East, West };
 
 fn solve(input: []const u8, comptime part: usize) !usize {
     _ = part;
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    var lines = std.mem.splitSequence(u8, input, "\r\n");
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
 
     var platform = std.ArrayList([]u8).init(allocator);
 
     while (lines.next()) |line| {
+        if (line.len == 0) continue;
         try platform.append(try allocator.dupe(u8, line));
     }
 
     var load: usize = 0;
 
-    print(&platform);
+    var pcache = std.ArrayList(std.ArrayListUnmanaged([]u8)).init(allocator);
 
-    // for (1..platform.items.len) |j| {
-    //     for (0..platform.items[0].len) |i| {
-    //         const c = if (direction == Direction.North) platform.items[j][i] else platform.items[platform.items.len - 1 - j][i];
+    var found: usize = 0;
+    outer: for (0..1000) |_| {
+        try pcache.append(try std.ArrayListUnmanaged([]u8).initCapacity(allocator, platform.items.len));
+        var copy = &pcache.items[pcache.items.len - 1];
+        for (platform.items) |src| {
+            try copy.append(allocator, try allocator.dupe(u8, src));
+        }
 
-    //         if (c != 'O') continue;
+        for (0..4) |i| {
+            Cycle(i, &platform);
+        }
 
-    //         for (0..j) |k| { // North tilt
-    //             if (platform.items[j - k - 1][i] == '.') {
-    //                 std.mem.swap(u8, &platform.items[j - k][i], &platform.items[j - k - 1][i]);
-    //             } else break;
-    //         }
-
-    //         for (0..j) |k| { // South tilt
-    //             if (platform.items[platform.items.len - j + k][i] == '.') {
-    //                 std.mem.swap(u8, &platform.items[platform.items.len - 1 - j + k][i], &platform.items[platform.items.len - j + k][i]);
-    //             } else break;
-    //         }
-    //     }
-    // }
-
-    // for (0..platform.items.len) |j| {
-    //     for (1..platform.items[0].len) |i| {
-    //         // const c = platform.items[j][i];
-
-    //         const c = if (direction == Direction.West) platform.items[j][i] else platform.items[j][platform.items[j].len - 1 - i];
-
-    //         if (c != 'O') continue;
-
-    //         // for (0..i) |k| { // West tilt
-    //         //     if (platform.items[j][i - k - 1] == '.') {
-    //         //         std.mem.swap(u8, &platform.items[j][i - k], &platform.items[j][i - k - 1]);
-    //         //     } else break;
-    //         // }
-
-    //         for (0..i) |k| { // East tilt
-    //             if (platform.items[j][platform.items[j].len - i + k] == '.') {
-    //                 std.mem.swap(u8, &platform.items[j][platform.items[j].len - 1 - i + k], &platform.items[j][platform.items[j].len - i + k]);
-    //             } else break;
-    //         }
-    //     }
-    // }
-    const dir = Direction.North;
-    Cycle(dir, &platform);
-
-    print(&platform);
-
-    for (platform.items, 0..) |line, i| {
-        for (line) |c| {
-            if (c == 'O') load += (platform.items.len - i);
+        for (pcache.items, 0..) |tmp, k| {
+            if (PlatformEqual(platform.items, tmp.items)) {
+                // print(pcache.items[pcache.items.len - 1].items);
+                found = k;
+                break :outer;
+            }
         }
     }
+
+    for (pcache.items, 0..) |*c, m| {
+        std.debug.print("{d}, {d}\n", .{ m, ComputeLoad(c.items) });
+        print(c.items);
+    }
+
+    const offset = (1000000000 - found) % (pcache.items.len - found);
+    load = ComputeLoad(pcache.items[offset + found].items);
 
     return load;
 }
 
-fn print(array: *std.ArrayList([]u8)) void {
-    for (array.items) |line| std.debug.print("{s}\n", .{line});
+fn ComputeLoad(platform: [][]u8) usize {
+    var load: usize = 0;
+    for (platform, 0..) |line, i| {
+        for (line) |c| {
+            if (c == 'O') load += (line.len - i);
+        }
+    }
+    return load;
+}
+
+fn PlatformEqual(lhs: [][]u8, rhs: [][]u8) bool {
+    for (lhs, rhs) |l, r| {
+        if (!std.mem.eql(u8, l, r)) return false;
+    }
+    return true;
+}
+
+fn print(lines: [][]u8) void {
+    for (lines) |line| std.debug.print("{s}\n", .{line});
     std.debug.print("\n", .{});
 }
 
-fn Cycle(direction: Direction, platform: *std.ArrayList([]u8)) void {
-    const sj = if (direction == Direction.North or direction == Direction.South) 1 else 0;
-    const si = if (direction == Direction.West or direction == Direction.East) 1 else 0;
+fn Cycle(direction: usize, platform: *std.ArrayList([]u8)) void {
+    const sj: usize = if (direction == 0 or direction == 2) 1 else 0;
+    const si: usize = if (direction == 1 or direction == 3) 1 else 0;
 
     for (sj..platform.items.len) |j| {
         for (si..platform.items[0].len) |i| {
             const c = switch (direction) {
-                Direction.North, Direction.West => platform.items[j][i],
-                Direction.South => platform.items[platform.items.len - 1 - j][i],
-                Direction.East => platform.items[j][platform.items[j].len - 1 - i],
+                0, 1 => platform.items[j][i],
+                2 => platform.items[platform.items.len - 1 - j][i],
+                3 => platform.items[j][platform.items[j].len - 1 - i],
+                else => unreachable,
             };
 
             if (c != 'O') continue;
 
             switch (direction) {
-                Direction.North => {
+                0 => {
                     for (0..j) |k| { // North tilt
                         if (platform.items[j - k - 1][i] == '.') {
                             std.mem.swap(u8, &platform.items[j - k][i], &platform.items[j - k - 1][i]);
                         } else break;
                     }
                 },
-                Direction.South => {
-                    for (0..j) |k| { // South tilt
-                        if (platform.items[platform.items.len - j + k][i] == '.') {
-                            std.mem.swap(u8, &platform.items[platform.items.len - 1 - j + k][i], &platform.items[platform.items.len - j + k][i]);
-                        } else break;
-                    }
-                },
-                Direction.West => {
+                1 => {
                     for (0..i) |k| { // West tilt
                         if (platform.items[j][i - k - 1] == '.') {
                             std.mem.swap(u8, &platform.items[j][i - k], &platform.items[j][i - k - 1]);
                         } else break;
                     }
                 },
-                Direction.East => {
+                2 => {
+                    for (0..j) |k| { // South tilt
+                        if (platform.items[platform.items.len - j + k][i] == '.') {
+                            std.mem.swap(u8, &platform.items[platform.items.len - 1 - j + k][i], &platform.items[platform.items.len - j + k][i]);
+                        } else break;
+                    }
+                },
+                3 => {
                     for (0..i) |k| { // East tilt
                         if (platform.items[j][platform.items[j].len - i + k] == '.') {
                             std.mem.swap(u8, &platform.items[j][platform.items[j].len - 1 - i + k], &platform.items[j][platform.items[j].len - i + k]);
                         } else break;
                     }
                 },
+                else => unreachable,
             }
         }
     }
