efce75df52177d390fb467eac806177f7c803fef
LilWeavs <andrewjw827@gmail.com>
2024-01-11 09:00:34 -0600

    wip interval bug fix

--- day19/main.zig
+++ day19/main.zig
@@ -3,27 +3,21 @@ const std = @import("std");
 pub fn main() !void {
     const part1 = try solve(@embedFile("test.txt"), 1);
     std.debug.print("Day 19|1: {d}\n", .{part1});
-    // const part2 = try solve(@embedFile("test.txt"), 2);
-    // std.debug.print("Day 19|2: {d}\n", .{part2});
+    const part2 = try solve(@embedFile("test.txt"), 2);
+    std.debug.print("Day 19|2: {d}\n", .{part2});
 }
 
 const Part = struct { x: usize, m: usize, a: usize, s: usize };
 
 const Instruction = struct { category: u8, cmp: u8, val: usize, rule: []const u8 };
 
-const Node = struct {
-    parent: ?*Node = null,
-    left: ?*Node = null,
-    right: ?*Node = null,
-    ins: Instruction,
-};
-
 fn solve(input: []const u8, comptime part: usize) !usize {
-    _ = part;
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
     var lines = std.mem.splitSequence(u8, input, "\r\n");
+    // var lines = std.mem.splitScalar(u8, input, '\n');
+    // var lines = std.mem.tokenizeSequence(u8, input, "\r\n");
 
     var workflows = std.StringHashMap(std.ArrayList(Instruction)).init(allocator);
 
@@ -64,84 +58,138 @@ fn solve(input: []const u8, comptime part: usize) !usize {
         });
     }
 
-    var sum: usize = 0;
-    for (parts.items) |prt| {
-        var workflow = workflows.get("in").?;
+    std.debug.print("Workflows: {d}\n", .{workflows.capacity()});
+    std.debug.print("Parts: {d}\n", .{parts.items.len});
 
-        // std.debug.print("Part: {{x={d},m={d},a={d},s={d}}}\n", .{ prt.x, prt.m, prt.a, prt.s });
-        var i: usize = 0;
-        const ack = while (true) {
-            const plan = workflow.items[i];
-            // std.debug.print("Plan: {c}{c}{d}:{s}\n", .{ plan.category, plan.cmp, plan.val, plan.rule });
-            var tmp: bool = false;
-            if (plan.cmp == '<') {
-                tmp = switch (plan.category) {
-                    'x' => prt.x < plan.val,
-                    'm' => prt.m < plan.val,
-                    'a' => prt.a < plan.val,
-                    's' => prt.s < plan.val,
-                    else => unreachable,
-                };
-            } else if (plan.cmp == '>') {
-                tmp = switch (plan.category) {
-                    'x' => prt.x > plan.val,
-                    'm' => prt.m > plan.val,
-                    'a' => prt.a > plan.val,
-                    's' => prt.s > plan.val,
-                    else => unreachable,
-                };
-            } else tmp = true;
+    if (comptime part == 1) {
+        var sum: usize = 0;
+        for (parts.items) |prt| {
+            var workflow = workflows.get("in").?;
 
-            if (tmp) {
-                if (plan.rule.len == 1 and plan.rule[0] == 'A') {
-                    break true;
-                } else if (plan.rule.len == 1 and plan.rule[0] == 'R') {
-                    break false;
-                } else {
-                    workflow = workflows.get(plan.rule).?;
-                    i = 0;
-                }
-            } else i += 1;
-        } else unreachable;
+            // std.debug.print("Part: {{x={d},m={d},a={d},s={d}}}\n", .{ prt.x, prt.m, prt.a, prt.s });
+            var i: usize = 0;
+            const ack = while (true) {
+                const plan = workflow.items[i];
+                // std.debug.print("Plan: {c}{c}{d}:{s}\n", .{ plan.category, plan.cmp, plan.val, plan.rule });
+                var tmp: bool = false;
+                if (plan.cmp == '<') {
+                    tmp = switch (plan.category) {
+                        'x' => prt.x < plan.val,
+                        'm' => prt.m < plan.val,
+                        'a' => prt.a < plan.val,
+                        's' => prt.s < plan.val,
+                        else => unreachable,
+                    };
+                } else if (plan.cmp == '>') {
+                    tmp = switch (plan.category) {
+                        'x' => prt.x > plan.val,
+                        'm' => prt.m > plan.val,
+                        'a' => prt.a > plan.val,
+                        's' => prt.s > plan.val,
+                        else => unreachable,
+                    };
+                } else tmp = true;
 
-        if (ack) {
-            sum += (prt.x + prt.m + prt.a + prt.s);
+                if (tmp) {
+                    if (plan.rule.len == 1 and plan.rule[0] == 'A') {
+                        break true;
+                    } else if (plan.rule.len == 1 and plan.rule[0] == 'R') {
+                        break false;
+                    } else {
+                        workflow = workflows.get(plan.rule).?;
+                        i = 0;
+                    }
+                } else i += 1;
+            } else unreachable;
+
+            if (ack) {
+                sum += (prt.x + prt.m + prt.a + prt.s);
+            }
         }
+        return sum;
     }
 
     // create binary tree of poosibilities
-    var tree = Node{ .parent = null, .left = undefined, .right = undefined, .ins = Instruction{ .category = '0', .cmp = '0', .val = 0, .rule = "in" } };
+    const possibilies = TraverseTree("in", &workflows, Range{ .l = 1, .r = 4000 }, Range{ .l = 1, .r = 4000 }, Range{ .l = 1, .r = 4000 }, Range{ .l = 1, .r = 4000 });
 
-    try AddNode(&tree, 0, workflows, allocator);
-
-    // for (workflow.items) |plan| {
-    //     tree = Node{ .parent = null, .left = undefined, .right = undefined, .ins = plan };
-    // }
-    return sum;
+    return possibilies;
 }
 
-fn AddNode(parent: *Node, idx: usize, workflows: std.StringHashMap(std.ArrayList(Instruction)), allocator: std.mem.Allocator) !void {
-    if (parent.ins.rule.len == 1) return;
-    var workflow = workflows.get(parent.ins.rule).?;
+const Range = struct { l: usize, r: usize };
 
-    var plan = workflow.items[idx];
-    std.debug.print("{c}{c}{d}:{s}\n", .{ plan.category, plan.cmp, plan.val, plan.rule });
-    var left = try allocator.create(Node);
-    left.parent = parent;
-    left.ins = plan;
-    parent.left = left;
-    try AddNode(left, 0, workflows, allocator);
-
-    // right means failed
-    var right = try allocator.create(Node);
-    right.parent = parent;
-    right.ins = plan;
-    parent.right = right;
-
-    // either reject or ack or goto next
-    if (plan.cmp == '0') {
-        try AddNode(right, 0, workflows, allocator);
+fn GetInterval(r: Range, cmp: u8, val: usize) Range {
+    var range = r;
+    if (cmp == '<') {
+        if (range.r > val) range.r = val - 1;
     } else {
-        try AddNode(right, idx + 1, workflows, allocator);
+        if (range.l < val) range.l = val + 1;
     }
+    return range;
+}
+
+fn TraverseTree(
+    rule: []const u8,
+    workflows: *std.StringHashMap(std.ArrayList(Instruction)),
+    xrange: Range,
+    mrange: Range,
+    arange: Range,
+    srange: Range,
+) usize {
+    var xr = xrange;
+    var mr = mrange;
+    var ar = arange;
+    var sr = srange;
+    if (rule.len == 1 and rule[0] == 'A') {
+        std.debug.print("[{},{}], [{},{}], [{},{}], [{},{}]\n", .{ xr.l, xr.r, mr.l, mr.r, ar.l, ar.r, sr.l, sr.r });
+        return (xr.r - xr.l + 1) * (mr.r - mr.l + 1) * (ar.r - ar.l + 1) * (sr.r - sr.l + 1); // accepted
+    } else if (rule.len == 1 and rule[0] == 'R') return 0; // rejected
+    const workflow = workflows.get(rule).?;
+
+    var possibilities: usize = 0;
+    for (workflow.items) |plan| {
+        if (plan.cmp != '0') {
+            switch (plan.category) {
+                'x' => {
+                    if (plan.cmp == '<') {
+                        possibilities += TraverseTree(plan.rule, workflows, Range{ .l = xr.l, .r = plan.val - 1 }, mr, ar, sr);
+                        xr.l = plan.val;
+                    } else {
+                        possibilities += TraverseTree(plan.rule, workflows, Range{ .l = plan.val + 1, .r = mr.r }, mr, ar, sr);
+                        xr.r = plan.val;
+                    }
+                },
+                'm' => {
+                    if (plan.cmp == '<') {
+                        possibilities += TraverseTree(plan.rule, workflows, xr, Range{ .l = mr.l, .r = plan.val - 1 }, ar, sr);
+                        mr.l = plan.val;
+                    } else {
+                        possibilities += TraverseTree(plan.rule, workflows, xr, Range{ .l = plan.val + 1, .r = mr.r }, ar, sr);
+                        mr.r = plan.val;
+                    }
+                },
+                'a' => {
+                    if (plan.cmp == '<') {
+                        possibilities += TraverseTree(plan.rule, workflows, xr, mr, Range{ .l = ar.l, .r = plan.val - 1 }, sr);
+                        ar.l = plan.val;
+                    } else {
+                        possibilities += TraverseTree(plan.rule, workflows, xr, mr, Range{ .l = plan.val + 1, .r = ar.r }, sr);
+                        ar.r = plan.val;
+                    }
+                },
+                's' => {
+                    if (plan.cmp == '<') {
+                        possibilities += TraverseTree(plan.rule, workflows, xr, mr, ar, Range{ .l = sr.l, .r = plan.val - 1 });
+                        sr.l = plan.val;
+                    } else {
+                        possibilities += TraverseTree(plan.rule, workflows, xr, mr, ar, Range{ .l = plan.val + 1, .r = sr.r });
+                        sr.r = plan.val;
+                    }
+                },
+                else => unreachable,
+            }
+        } else {
+            possibilities += TraverseTree(plan.rule, workflows, xr, mr, ar, sr);
+        }
+    }
+    return possibilities;
 }
