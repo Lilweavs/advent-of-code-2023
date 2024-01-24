90a6a814b3eb2d79cb87eae19ae5e3c843c725b9
saltymrbighead <35853304+saltymrbighead@users.noreply.github.com>
2024-01-23 22:58:31 -0600

    wip

--- day23/main.zig
+++ day23/main.zig
@@ -27,101 +27,130 @@ fn solve(input: []const u8, comptime part: usize) !usize {
     }
 
     const start = Point{ .x = 1, .y = 0 };
-    const end = Point{ .x = map.items[0].len - 1, .y = map.items.len - 2 };
+    const end = Point{ .x = map.items[0].len - 2, .y = map.items.len - 1 };
+
+    std.debug.print("End: {d},{d}\n", .{ end.y, end.x });
 
     var path = std.ArrayList(Point).init(allocator);
     try path.append(start);
+    try path.append(Point{ .x = start.x, .y = start.y + 1 });
 
     // every time we find a split store it
     var pathSplits = std.ArrayList(Split).init(allocator);
 
-    try pathSplits.append(Split{ .idx = 0, .c = 'v' });
+    // try pathSplits.append(Split{ .idx = 0, .c = 'v' });
 
-    var curr = start;
-    while (pathSplits.items.len != 0) {
-        const split = pathSplits.pop();
+    var numPaths: usize = 0;
+    while (numPaths == 0 or pathSplits.items.len != 0) : (numPaths += 1) {
+        var curr = path.getLast();
+        // const split = pathSplits.popOrNull();
 
-        std.debug.print("Split: {c}\n", .{split.c});
+        if (pathSplits.popOrNull()) |split| {
+            std.debug.print("Split: {c},{d}\n", .{ split.c, split.idx });
 
-        try path.resize(split.idx + 1);
+            try path.resize(split.idx + 1);
 
-        switch (split.c) {
-            'v' => try path.append(.{ .x = curr.x, .y = curr.y + 1 }),
-            '>' => try path.append(.{ .x = curr.x + 1, .y = curr.y }),
-            else => unreachable,
+            switch (split.c) {
+                'v' => try path.append(.{ .x = curr.x, .y = curr.y + 1 }),
+                '>' => try path.append(.{ .x = curr.x + 1, .y = curr.y }),
+                else => unreachable,
+            }
         }
 
-        curr = path.items[path.items.len - 1];
+        curr = path.getLast();
 
         outer: while (true) {
-            if (map.items[curr.y + 1][curr.x] == '.' or map.items[curr.y + 1][curr.x] == 'v') {
+
+            // down is not in path
+            if (path.items[path.items.len - 2].y != curr.y + 1 and map.items[curr.y + 1][curr.x] != '#') {
                 for (curr.y + 1..map.items.len) |j| {
-                    if (curr.y == end.y and curr.x == end.x) {
-                        std.debug.print("PathLength: {d}", .{path.items.len});
+                    if (j == end.y and curr.x == end.x) {
+                        std.debug.print("PathLength: {d}\n", .{path.items.len});
                         break :outer;
                     }
 
                     const pt = map.items[j][curr.x];
-                    std.debug.print("D: {c},{d},{d}\n", .{ pt, j, curr.x });
                     if (pt == '.' or pt == 'v') {
+                        std.debug.print("D: {c},{d},{d}\n", .{ pt, j, curr.x });
                         try path.append(Point{ .x = curr.x, .y = j });
                     } else {
                         curr.y = j - 1;
                         break;
                     }
 
-                    if (map.items[j - 1][curr.x] == 'v' and (map.items[j - 1][curr.x] == 'v' or map.items[j - 1][curr.x] == '#')) {
-                        if (map.items[j + 1][curr.x] == 'v') {
-                            try pathSplits.append(.{ .idx = path.items.len, .c = 'v' });
+                    curr = path.getLast();
+                    if (map.items[j - 1][curr.x] == 'v' and map.items[j + 1][curr.x] == 'v') {
+                        if (map.items[j][curr.x + 1] == '>') {
+                            try pathSplits.append(.{ .idx = path.items.len, .c = '>' });
                         }
-                        try pathSplits.append(.{ .idx = path.items.len, .c = '>' });
-                        break :outer;
                     }
                 }
-            } else if (map.items[curr.y][curr.x + 1] == '.') {
+            }
+            curr = path.getLast();
+
+            // right is not in path
+            if (path.items[path.items.len - 1].x != curr.x + 1 and map.items[curr.y][curr.x + 1] != '#') {
                 for (curr.x + 1..map.items[0].len) |i| {
                     const pt = map.items[curr.y][i];
-                    std.debug.print("R: {c},{d},{d}\n", .{ pt, curr.y, i });
                     if (pt == '.' or pt == '>') {
+                        std.debug.print("R: {c},{d},{d}\n", .{ pt, curr.y, i });
                         try path.append(Point{ .x = i, .y = curr.y });
                     } else {
                         curr.x = i - 1;
                         break;
                     }
 
-                    if (map.items[curr.y][i - 1] == '>' and (map.items[curr.y][i + 1] == '>' or map.items[curr.y][i + 1] == '#')) {
-                        if (map.items[curr.y][i + 1] == '>') {
-                            try pathSplits.append(.{ .idx = path.items.len, .c = '>' });
+                    if (map.items[curr.y][i - 1] == '>' and map.items[curr.y][i + 1] == '>') {
+                        if (map.items[curr.y + 1][i] == 'v') {
+                            try pathSplits.append(.{ .idx = path.items.len, .c = 'v' });
                         }
-                        try pathSplits.append(.{ .idx = path.items.len, .c = 'v' });
-                        break :outer;
                     }
                 }
-            } else if (map.items[curr.y][curr.x - 1] == '.') {
+            }
+            curr = path.getLast();
+            var prev = path.items[path.items.len - 2];
+            // std.debug.print("curr: {d},{d}\n", .{ curr.y, curr.x });
+            // std.debug.print("curr: {d},{d}\n", .{ curr.y, curr.x });
+            // std.debug.print("prev: {d},{d}\n", .{ prev.y, prev.x });
+            // left is not in path
+            if (path.items[path.items.len - 2].x != curr.x - 1 and map.items[curr.y][curr.x - 1] != '#') {
                 for (1..curr.x) |i| {
                     const pt = map.items[curr.y][curr.x - i];
-                    std.debug.print("L: {c},{d},{d}\n", .{ pt, curr.y, curr.x - i });
                     if (pt == '.' or pt == '>') {
+                        std.debug.print("L: {c},{d},{d}\n", .{ pt, curr.y, curr.x - i });
                         try path.append(Point{ .x = curr.x - i, .y = curr.y });
                     } else {
                         curr.x = curr.x - i + 1;
                         break;
                     }
                 }
-            } else if (map.items[curr.y - 1][curr.x] == '.') {
+            }
+
+            curr = path.getLast();
+            prev = path.items[path.items.len - 2];
+            std.debug.print("curr: {d},{d}\n", .{ curr.y, curr.x });
+            std.debug.print("prev: {d},{d}\n", .{ prev.y, prev.x });
+            // left is not in path
+            if (path.items[path.items.len - 2].y != curr.y - 1 and map.items[curr.y - 1][curr.x] != '#') {
                 for (1..curr.y) |j| {
                     const pt = map.items[curr.y - j][curr.x];
-                    std.debug.print("U: {c},{d},{d}\n", .{ pt, curr.y - j, curr.x });
                     if (pt == '.') {
+                        std.debug.print("U: {c},{d},{d}\n", .{ pt, curr.y - j, curr.x });
                         try path.append(Point{ .x = curr.x, .y = curr.y - j });
                     } else {
                         curr.y = curr.y - j + 1;
                         break;
                     }
                 }
-            } else {
-                unreachable;
             }
+
+            // if (map.items[curr.y + 1][curr.x] == '.' or map.items[curr.y + 1][curr.x] == 'v') {}
+
+            // if (map.items[curr.y][curr.x + 1] == '.') {} else if (map.items[curr.y][curr.x - 1] == '.') {
+            // } else if (map.items[curr.y - 1][curr.x] == '.') {
+            // } else {
+            //     unreachable;
+            // }
         }
     }
 
